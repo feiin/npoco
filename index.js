ae5f6d272f9a282acef6d9a7d594587effacf8a1
feiin <feiin@qq.com>
2015-10-19 14:36:35 +0800

    add render engine with ejs

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ var npoco = require("./lib/npoco.js");
 
 console.log('hello this is npoco');
 console.log(process.argv);
-
+console.log(process.cwd());
+console.log(__dirname);
 npoco.renderCode({"schemaName":"dollor"});
